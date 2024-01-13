c90677177866a02e2b72270f84b328ea3dfc0a45
koldassovnt <koldassovnt@gmail.com>
2024-01-13 12:07:47 +0600

    added BinarySearch with recursion

--- src/grokaem_algo_book/BinarySearch.java
+++ src/grokaem_algo_book/BinarySearch.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@SuppressWarnings("DuplicatedCode")
 public class BinarySearch {
 
     private static int binarySearch(List<Integer> list, Integer guessedNum) {
 
         int low = 0;
         int high = list.size(); // 1000
+
+        if (guessedNum > high) {
+            throw new RuntimeException("0VbxAxh4 :: guessed number does not exists in the list ");
+        }
 
         while (low <= high) {
 
@@ -29,7 +34,31 @@ public class BinarySearch {
 
         }
 
-        throw new RuntimeException("0VbxAxh4 :: guessed number does not exists in the list ");
+        throw new RuntimeException("35cL61kD :: Wrong algorithm work");
+
+    }
+
+    private static int binarySearch(List<Integer> list, Integer guessedNum, int low, int high) {
+
+        if (guessedNum > list.size()) {
+            throw new RuntimeException("vD8DwWe0 :: guessed number does not exists in the list ");
+        }
+
+        int mid = (low + high) / 2;
+
+        Integer guess = list.get(mid);
+
+        if (Objects.equals(guess, guessedNum)) {
+            return mid;
+        }
+
+        if (guess > guessedNum) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+
+        return binarySearch(list, guessedNum, low, high);
 
     }
 
@@ -45,7 +74,11 @@ public class BinarySearch {
 
         int index = binarySearch(list, guessedNum);
 
-        System.out.println(index);
+        System.out.println("2Cl9KU29 :: index = " + index);
+
+        int index2 = binarySearch(list, guessedNum, 0, list.size());
+
+        System.out.println("C9QTElNi :: index 2 = " + index2);
 
     }
 
