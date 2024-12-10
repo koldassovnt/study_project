1e7067caff899d1c96200eb5712329e0d0fcf7e4
nkoldassov <koldassovnt@gmail.com>
2024-12-10 23:04:14 +0500

    added leetcode solution: RemoveDuplicatesFromSortedArr2

---
+++ src/leetcode/medium/RemoveDuplicatesFromSortedArr2.java
@@ -0,0 +1,88 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums sorted in non-decreasing order,
+ * remove some duplicates in-place such that each unique element appears at most twice.
+ * The relative order of the elements should be kept the same.
+ * <p>
+ * Since it is impossible to change the length of the array in some languages,
+ * you must instead have the result be placed in the first part of the array nums.
+ * More formally, if there are k elements after removing the duplicates,
+ * then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+ * <p>
+ * Return k after placing the final result in the first k slots of nums.
+ * <p>
+ * Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * Custom Judge:
+ * <p>
+ * The judge will test your solution with the following code:
+ * <p>
+ * int[] nums = [...]; // Input array
+ * int[] expectedNums = [...]; // The expected answer with correct length
+ * <p>
+ * int k = removeDuplicates(nums); // Calls your implementation
+ * <p>
+ * assert k == expectedNums.length;
+ * for (int i = 0; i < k; i++) {
+ * assert nums[i] == expectedNums[i];
+ * }
+ * If all assertions pass, then your solution will be accepted.
+ * <p>
+ * Input: nums = [1,1,1,2,2,3]
+ * Output: 5, nums = [1,1,2,2,3,_]
+ * Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ * <p>
+ * Input: nums = [0,0,1,1,1,1,2,3,3]
+ * Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+ * Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+public class RemoveDuplicatesFromSortedArr2 {
+
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums.length == 2) {
+            return 2;
+        }
+
+        int j = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+
+            if (nums[i] != nums[j - 2]) {
+                nums[j] = nums[i];
+                j++;
+            }
+
+        }
+
+        return j;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 1, 1, 2, 2, 3};
+        int[] arr2 = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        int[] arr3 = {1, 1};
+        int[] arr4 = {1, 1, 1, 1};
+
+        System.out.println(removeDuplicates(arr1));
+        System.out.println(Arrays.toString(arr1));
+
+        System.out.println(removeDuplicates(arr2));
+        System.out.println(Arrays.toString(arr2));
+
+        System.out.println(removeDuplicates(arr3));
+        System.out.println(Arrays.toString(arr3));
+
+        System.out.println(removeDuplicates(arr4));
+        System.out.println(Arrays.toString(arr4));
+
+    }
+
+}
